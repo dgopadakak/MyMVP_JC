1763608fa2f7d4c3a542feaf5e44cf62c1db4e82
dgopa <dgopadakakv3@gmail.com>
2023-09-26 22:14:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mymvpjc/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.mymvpjc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mymvpjc.ui.theme.MyMVPJCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyMVPJCTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+//    MyMVPJCTheme {
+//        Greeting("Android")
+//    }
+//
+    Box(modifier = Modifier
+        .background(Color.Red)
+        .size(200.dp)
+        .padding(32.dp)
+        .background(Color.Green)
+        .padding(32.dp)
+        .background(Color.Blue)
+    )
+}
