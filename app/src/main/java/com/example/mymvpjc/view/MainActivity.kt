47812b53c58a3a2e1885edea2252f74f50208a0d
dgopa <dgopadakakv3@gmail.com>
2023-09-28 13:24:54 +0300

    Experiments with Jetpack Compose's Surface

--- app/src/main/java/com/example/mymvpjc/view/MainActivity.kt
+++ app/src/main/java/com/example/mymvpjc/view/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.mymvpjc.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mymvpjc.appComponent
 import com.example.mymvpjc.presenter.Presenter
 import com.example.mymvpjc.ui.theme.MyMVPJCTheme
@@ -67,7 +75,7 @@ class MainActivity : ComponentActivity(), Viewable {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
@@ -81,9 +89,20 @@ fun GreetingPreview() {
 //        ) {
 //            Greeting(
 //                name = "Android",
-//                modifier = Modifier.padding(32.dp)
+//                modifier = Modifier
+//                    .padding(32.dp)
+//                    //.background(color = Color.Green, shape = CircleShape)
 //            )
 //        }
+//    }
+//
+//    MyMVPJCTheme {
+//        Greeting(
+//            name = "Android",
+//            modifier = Modifier.padding(32.dp)
+//                .fillMaxSize()
+//                .background(color = Color.Green, shape = CircleShape)
+//        )
 //    }
 //
 //    Box(modifier = Modifier
@@ -95,5 +114,21 @@ fun GreetingPreview() {
 //        .background(Color.Blue)
 //    )
 //
+//    MyMVPJCTheme {
+////        Surface (
+////            modifier = Modifier.fillMaxSize(),
+////            color = MaterialTheme.colorScheme.background
+////        ) {
+////            Text(text = "aaaa", modifier = Modifier.padding(20.dp).background(Color.Blue))
+////            Text(text = "bbbb", modifier = Modifier.padding(150.dp).background(Color.Red))
+////        }
+//
+//        Box (
+//            modifier = Modifier.fillMaxSize()
+//        ) {
+//            Text(text = "aaaa", modifier = Modifier.padding(20.dp).background(Color.Blue))
+//            Text(text = "bbbb", modifier = Modifier.padding(150.dp).background(Color.Red))
+//        }
+//    }
 
 }
